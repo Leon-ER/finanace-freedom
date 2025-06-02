9b79de5c37b863a69e5985520f7b35874ac62a5b
Leon-ER <leonerdesi2004@gmail.com>
2025-06-02 15:56:06 -0400

    Folder structure for project and middleware

---
+++ src/middleware.js
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+
+export function middleware(request) {
+    console.log("Running")
+    
+    // const user =  "logged";
+
+    // if(!user){
+    //     return NextResponse.redirect(
+    //         new URL('/', req.url)
+    //     )
+    // }
+
+    return NextResponse.next();
+}
+
+// export const config = {
+//     matcher: ['/dashboard', '/goals', '/reports', '/transactions', '/settings']
+// }
